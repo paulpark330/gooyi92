44149474df8793d25637f2573ab517c48f9e5650
Paul Park <paulpark330@hotmail.com>
2021-10-11 20:36:09 -0700

    update

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 /** @format */
 
-const form = document.querySelector('#form');
+const $registerForm = document.querySelector('#register-form');
 let $netSales = document.querySelector('#net-sales');
 let $bossMoney = document.querySelector('#boss-money');
+let $registerMoney = document.querySelector('#register-money')
 
 let netSales,
   bossMoney,
-  delivery = 0;
+  delivery,
+  registerMoney
 
 let tip = 0;
 let payout = 0;
@@ -20,7 +22,7 @@ let fantuan = 0;
 
 const handleSubmit = (e) => {
   e.preventDefault();
-  const formData = new FormData(form);
+  const formData = new FormData($registerForm);
   tip = parseFloat(formData.get('tip'));
   payout = parseFloat(formData.get('payout'));
   openMoney = parseFloat(formData.get('open-money'));
@@ -33,10 +35,10 @@ const handleSubmit = (e) => {
   netSales = (ccSales + cashSales - tax).toFixed(2);
   delivery = (doordash + grubhub + fantuan).toFixed(2);
   bossMoney = (cashSales - tip - payout - delivery).toFixed(2);
+  registerMoney = (openMoney + cashSales - tip - payout).toFixed(2);
   $netSales.value = netSales;
   $bossMoney.value = bossMoney;
+  $registerMoney.value  = registerMoney;
 };
 
 form.addEventListener('submit', handleSubmit);
-
-console.log('hi')
